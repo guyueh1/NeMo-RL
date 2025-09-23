2d65579057d034acbb3131c21dce3e882f5282ef
Guyue Huang <guyueh@nvidia.com>
2025-09-23 13:15:55 -0700

    Fix no module named megatron.training.datasets
    Signed-off-by: Guyue Huang <guyueh@nvidia.com>

--- nemo_rl/algorithms/grpo.py
+++ nemo_rl/algorithms/grpo.py
@@ -946,6 +946,7 @@ def grpo_train(
 
             print(f"  • Loss: {metrics['loss']:.4f}")
             print(f"  • Avg Reward: {np.mean(rewards.numpy()):.4f}")
+            print(f"  • Token mult prob error: {metrics['token_mult_prob_error']:.4f}")
             print(
                 f"  • Mean Generation Length: {rollout_metrics['mean_gen_tokens_per_sample']:.4f}",
                 flush=True,
