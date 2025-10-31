02698c98e0cf8a4b9567f5149d2c2aaf6daba713
Guyue Huang <guyueh@nvidia.com>
2025-10-31 11:57:00 -0700

    fix
    Signed-off-by: Guyue Huang <guyueh@nvidia.com>

--- nemo_rl/utils/sequence_length_generator.py
+++ nemo_rl/utils/sequence_length_generator.py
@@ -11,10 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import numpy as np
 from typing import Callable
+
+import numpy as np
+
 
 def get_sequence_length_generator(sequence_length_generator_cfg: dict) -> Callable:
     mean = sequence_length_generator_cfg["mean"]
     std = sequence_length_generator_cfg["std"]
-    return lambda: int(np.round(np.random.normal(mean, std)))
+    length = int(np.round(np.random.normal(mean, std)))
+    length = max(1, length)
+    return length
